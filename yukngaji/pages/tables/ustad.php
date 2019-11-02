0bf455f6c751ed0135904b4e9519fc8eb0721ebd
royyan1207070 <royyan120707@gmail.com>
2019-11-02 21:05:00 +0700

    beta 0.4 nav-link active

--- yukngaji/pages/tables/ustad.php
+++ yukngaji/pages/tables/ustad.php
@@ -372,13 +372,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="../tables/ustad.php" class="nav-link">
+                <a href="../tables/ustad.php" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Data Ustad</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="../tables/santri.php" class="nav-link active">
+                <a href="../tables/santri.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Data Santri</p>
                 </a>
